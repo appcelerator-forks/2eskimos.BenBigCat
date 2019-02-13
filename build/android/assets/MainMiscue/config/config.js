37cfed7f2e448fb11479360193291ea15a089249
ben Barton <Ben@2eskimos.onmicrosoft.com>
2019-02-13 09:28:38 +0000

    Added keystore

--- build/android/assets/MainMiscue/config/config.js
+++ build/android/assets/MainMiscue/config/config.js
@@ -1,12 +1,1 @@
-/**
-* Appcelerator Titanium Platform
-* Copyright (c) 2009-2011 by Appcelerator, Inc. All Rights Reserved.
-* Licensed under the terms of the Apache Public License
-* Please see the LICENSE included with this distribution for details.
-**/
-
-//make a copy of this file as config.js, and add your Twitter xAuth-enabled keys here
-tt.config = {
-	consumer_key: 'SVVOGeBDkfkDqhKLrnw',
-	consumer_secret: 'ne3czav3B5y9C0kpS3RWW4SGqosy1wQ7ggnLH6uNBiY'
-};
+tt.config={consumer_key:'SVVOGeBDkfkDqhKLrnw',consumer_secret:'ne3czav3B5y9C0kpS3RWW4SGqosy1wQ7ggnLH6uNBiY'};
